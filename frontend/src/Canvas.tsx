a5056305fa57117b1070626999888cda2f817a29
Andy Li <andyli@Andys-MacBook-Pro-8.local>
2023-10-31 16:41:11 -0400

    Smooth snapping but need to change spawn coords to be relative to the stage

--- frontend/src/Canvas.tsx
+++ frontend/src/Canvas.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState, useRef, createRef } from "react";
 import Draggable from "react-draggable";
 import { Layer, Stage, Shape, Rect } from "react-konva";
+import { Easings } from "konva/lib/Tween";
+import "./index.css";
+import { spawn } from "child_process";
 
 interface Furniture {
   x: number;
@@ -8,8 +11,8 @@ interface Furniture {
   width: number;
   height: number;
   draggable: boolean;
-  fill: string;
-  stroke: string;
+  fill?: string;
+  stroke?: string;
 }
 
 interface Coords {
@@ -17,82 +20,103 @@ interface Coords {
   y: number;
 }
 
+// =======================
+// CANVAS PARAMS
+// =======================
+
+const gridSize = 20;
 const width = 600;
 const height = 800;
-const cols = 5;
-const rows = 5;
+const cols = height / gridSize;
+const rows = width / gridSize;
 
-export default function Canvas({}) {
+export default function Canvas() {
   const [furniture, setFurniture] = useState<Furniture[]>([]);
 
   const [refs, setRefs] = useState<any[]>([]);
 
-  // MouseCoords relative to Canvas
-  const [mouseCoords, setMouseCoords] = useState<Coords>({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState<boolean>(false);
 
-  // Spawner Original Coords for mind check
-  const [spawnerCoords, setSpawnerCoords] = useState<Coords>({x: 0, y: 0});
-  const spawnerRef = useRef(null);
+  // To avoid DOM Errors, attach to Draggable component
+  const nodeRef = useRef(null);
 
-  const spawnWidth = 200;
+  // To get coords of stage
+  const anchorRef = useRef(null);
 
-  // const spawnFurniture = (e: any) => {
-  //   setMouseCoords({
-  //     x: e.evt.clientX - 100,
-  //     y: e.evt.clientY - 100,
-  //   });
+  // Size of a furniture square in sidebar
+  const displayWidth = 40;
 
-  //   const newRect: Furniture = {
-  //     x: e.evt.clientX - 100,
-  //     y: e.evt.clientY - 100,
-  //     width: 200,
-  //     height: 200,
-  //     draggable: true,
-  //     fill: "#1e293b",
-  //     stroke: "#64748b",
-  //   };
+  // Size of a furniture square when spawned
+  const spawnWidth = 300;
 
-  //   setFurniture([...furniture, newRect]);
-  //   setRefs([...refs, createRef()]);
-  // };
-
+  // ****NOTE****
+  // Mouse event coords are based on mouse -> need to calculate rect coords
+  // Rect Coords are on top left corner
 
   const onFurnDragEnd = (e: any) => {
-    console.log(e)
+    console.log(anchorRef);
+    console.log(e.target.attrs.x);
 
-    let id = Number(e.target.attrs.id)
-    console.log(refs[id])
+    let id = Number(e.target.attrs.id);
+
+    // These coords are relevant to stage
+    let x = e.target.attrs.x;
+    let y = e.target.attrs.y;
+
+    if (x < 0) {
+      x = 0;
+    } else if (x + spawnWidth > width) {
+      x = width - spawnWidth;
+    } else {
+      let modX = x % gridSize;
+      x = x - modX + Math.round(modX / gridSize) * gridSize;
+    }
+
+    if (y < 0) {
+      y = 0;
+    } else if (y + spawnWidth > height) {
+      y = height - spawnWidth;
+    } else {
+      let modY = y % gridSize;
+      y = y - modY + Math.round(modY / gridSize) * gridSize;
+    }
+
+    refs[id]?.current.to({ x: x, y: y, easing: Easings.EaseInOut, duration: .5 });
+  };
+
+  const onDragStart = (e: any) => {
+    setDragging(true);
+  };
+
+  const onDragEnd = (e: any, data: any) => {
+    setDragging(false);
+    let dragStartCoords: Coords = {
+      x: e.pageX - e.layerX,
+      y: e.pageY - e.layerY,
+    };
+    console.log(e);
+    // console.log(dragStartCoords.x, dragStartCoords.y)
+
+    let dragEndCoords: Coords = {
+      x: dragStartCoords.x + data.x,
+      y: dragStartCoords.y + data.y,
+    };
 
 
-    // Client Coords are based on mouse
-    // Rect Coords are on top left corner
-
-    let x = e.evt.clientX - 100;
-    let y = e.evt.clientY - 100;
-
-    x = x - (x % 20);
-    y = y - (y % 20);
-    
-    refs[id]?.current.to({x: x, y: y})
-
-    console.log(x, y);
-}
-
-
-  const onDragEnd = (e:any, data:any) => {
-    // console.log(e, data);
-
-    // If out of bounds of canvas, then don't make anything
-    if (e.screenX > 800|| e.screenY > 800 ){
+    // If box limits are out of bounds of canvas, then don't make anything
+    if (
+      dragEndCoords.x + spawnWidth > 600 ||
+      dragEndCoords.y + spawnWidth > 800
+    ) {
       return;
     }
-    
+
     // Might need to adjust spawn coords if we edit dimensions of the Canvas
     const newRect: Furniture = {
-      x: e.pageX - 100,
-      y: e.pageY - 100,
-      width: 200,
-      height: 200,
+      x: dragEndCoords.x,
+      y: dragEndCoords.y,
+      width: spawnWidth,
+      height: spawnWidth,
       draggable: true,
       fill: "#1e293b",
       stroke: "#64748b",
@@ -100,8 +124,9 @@ export default function Canvas({}) {
 
     setFurniture([...furniture, newRect]);
     setRefs([...refs, createRef()]);
-  }
+  };
 
+  // ** DEBUGGING USE** //
   useEffect(() => {
     // console.log(furniture);
     // console.log(refs);
@@ -110,15 +135,38 @@ export default function Canvas({}) {
     // console.log(furniture);
   }, [furniture]);
 
+  // Handling spawning and snapping of a new furniture item
+  useEffect(() => {
+    if (refs.length !== 0) {
+      let x = furniture[furniture.length - 1].x;
+      let y = furniture[furniture.length - 1].y;
+
+      let modX = x % gridSize;
+      let modY = y % gridSize;
+
+      x = x - modX + Math.round(modX / gridSize) * gridSize;
+      y = y - modY + Math.round(modY / gridSize) * gridSize;
+
+      refs[refs.length - 1].current?.to({
+        x: x,
+        y: y,
+        duration: 0.5,
+        easing: Easings.EaseInOut,
+      });
+    }
+  }, [refs, furniture]);
 
   return (
-    <div style={{display: "flex", flexDirection: "row"}}>
+    <div className="flex flex-row justify-center items-center absolute top-0 w-full h-full">
       <Stage
-        width={width + spawnWidth}
+        width={width}
         height={height}
         style={{
           display: "flex",
           width: "fit-content",
+        }}
+        onMouseEnter={(e:any) => {
+          console.log(e);
         }}
       >
         <Layer>
@@ -129,6 +177,7 @@ export default function Canvas({}) {
             height={height}
             fill="#0f172a"
             shadowBlur={10}
+            ref={anchorRef}
           ></Rect>
           <Shape
             stroke="#1e293b"
@@ -137,14 +186,15 @@ export default function Canvas({}) {
               context.lineWidth = 4;
               context.strokeStyle = "#1e293b";
               context.beginPath();
-              for (var i = 0; i <= rows; i++) {
-                context.moveTo((width / cols) * i, 0);
-                context.lineTo((width / cols) * i, height);
+              let i = 0;
+              for (i = 0; i <= rows; i++) {
+                context.moveTo(gridSize * i, 0);
+                context.lineTo(gridSize * i, height);
                 context.stroke();
               }
-              for (var i = 0; i <= cols; i++) {
-                context.moveTo(0, (height / rows) * i);
-                context.lineTo(width, (height / rows) * i);
+              for (i = 0; i <= cols; i++) {
+                context.moveTo(0, gridSize * i);
+                context.lineTo(width, gridSize * i);
                 context.stroke();
               }
 
@@ -155,23 +205,51 @@ export default function Canvas({}) {
         <Layer>
           {/* Spawn Layer */}
           {furniture.map((furn: Furniture, i: number) => {
-            return <Rect id={`${i}`} {...furn} ref={refs[i]} onDragEnd={onFurnDragEnd}/>
+            return (
+              <Rect
+                key={i}
+                id={`${i}`}
+                {...furn}
+                ref={refs[i]}
+                onDragEnd={onFurnDragEnd}
+              />
+            );
           })}
         </Layer>
       </Stage>
-      <Draggable
-        position={{x: 0, y: 0}}
-        onStop={onDragEnd}
-      >
+      <div className="relative">
         <div
+          className="border-solid bg-slate-800 border-slate-100 border-2 absolute top-0 z-0 transition-all ease-in duration-200"
           style={{
-            border: '2px solid #64748b',
-            backgroundColor: "#1e293b",
-            width: 200,
-            height: 200,
+            width: displayWidth,
+            height: displayWidth,
+            opacity: dragging ? 0 : 1,
           }}
         ></div>
-      </Draggable>
+
+        <Draggable
+          position={{ x: 0, y: 0 }}
+          onStart={onDragStart}
+          onStop={onDragEnd}
+          nodeRef={nodeRef}
+        >
+          <div
+            ref={nodeRef}
+            className="z-1 absolute bg-slate-700"
+            style={{
+              opacity: dragging ? 1 : 0,
+            }}
+          >
+            <div
+              className="absolute top-0 border-solid bg-slate-800 border-slate-100 border-2 transition-all ease-in-out duration-500"
+              style={{
+                width: dragging ? spawnWidth : displayWidth,
+                height: dragging ? spawnWidth : displayWidth,
+              }}
+            ></div>
+          </div>
+        </Draggable>
+      </div>
     </div>
   );
 }
